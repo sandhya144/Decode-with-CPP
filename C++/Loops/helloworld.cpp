01caa7fa81ed53d9da13f3c9b47d46c554ba342e
sandhya144 <186sndy@gmail.com>
2024-07-15 23:28:36 +0530

    updated

--- C++/Loops/helloworld.cpp
+++ C++/Loops/helloworld.cpp
@@ -5,7 +5,7 @@ int n;
 cout<<"enter a no. : ";
 cin>>n;
 for(int i=1 ; i<=n ; i++){  // int i will works only for for loop 
-    cout<<"Hello World"<<endl; // otside the code block it will not run 
+    cout<<"Hello World"<<endl; // outside the code block it will not work
 }
 
 
--- C++/Loops/tableof19.cpp
+++ C++/Loops/tableof19.cpp
@@ -4,14 +4,14 @@ int main(){
 //  for (int i=19; i<=190; i++){
 //     if(i%19==0)  cout<<i<<endl;
 //  }
-// 190-19 = 171 times run kar raha hai...
 
-for (int i=19; i<=190; i+=19){
+// 190-19 = 171 times extra times loop run kar raha hai...
+
+for (int i=19; i<=190; i+=19){   // i=i+19 ---> 38 + 19
     cout<<i<<endl;
  }
 
 // 10 bar hi chal rha hai...
-
 
 
 }
